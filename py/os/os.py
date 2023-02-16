1e0e426e0e6406e72eaafd1fb44782b86f287c1c
Yana-Kh <ynakh2017@mail.ru>
2023-02-17 00:39:49 +0300

    os

---
+++ py/os/os.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+
+if __name__ == '__main__':
+    # Создание директории и запоминание пути
+    dir_name = input("Введите имя директории: ")
+    os.mkdir(dir_name)
+    path = os.getcwd()
+
+    #Перемещение в новую директорию
+    os.chdir(path + '\\' + dir_name)
+    with open("file.txt", "w", encoding="utf-8") as f:
+        # appending the content to the file
+        f.write(os.getcwd())
+
+    #Переименование файла
+    new_name = input("Введите новое имя для файла: ")
+    os.rename("file.txt", new_name)
